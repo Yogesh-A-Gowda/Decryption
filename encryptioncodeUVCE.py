472c37f3334332b79e8e0dae3cb4b2bb528de11d
Yogesh-A-Gowda <67061755+Yogesh-A-Gowda@users.noreply.github.com>
2022-05-20 20:12:51 +0530

    Update encryptioncodeUVCE.py

--- encryptioncodeUVCE.py
+++ encryptioncodeUVCE.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-
-class encryption:
-    
-   message = input("enter the encrypted code bruh, make sure everything is in smallcase only cause i dont want to complicate my code ")
-   x = message.lower()
-   for i in range(len(x)):
-        
-        a = ord(x[i])
-        if(a>109):
-            b = a-13
-            print(chr(b),end = " ")
-        elif(a<=109):
-            c = (26+a)-13
-            print(chr(c),end = " ")
-        else :
-            print(x[i])
-            
-
-
-    
-   
-            
-=======
 message = input("enter the encrypted code bruh, make sure everything is in smallcase only cause i dont want to complicate my code ")
 x = message.lower()
 for i in range(len(x)):
@@ -35,10 +11,3 @@ for i in range(len(x)):
             print(chr(c),end = " ")
         else :
             print(x[i])
-            
-
-
-    
-   
-            
->>>>>>> 2581c70082510496ee1eca0ee1ae50bc345dd55e
